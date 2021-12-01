0085cc14269c5f63b109b3d47b4eafbedc439d1f
cesar mayta <cesarmayta@gmail.com>
2021-11-30 22:24:33 -0500

    d6

--- menuya/menuya/settings.py
+++ menuya/menuya/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z^d1j-f^n8n9xj&m^3@bbe4)0e-r+y#u7%&-@bc$d*5@aeizp5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['18.188.189.14']
 
